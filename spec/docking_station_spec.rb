edea12940fa2a86c65b1041ada887e8dae003a51
Henry <hcvferrari@gmail.com>
2022-02-23 17:49:25 +0000

    DockingStation now releases bikes which respond to working? method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,12 @@
 require "docking_station"
 
 describe DockingStation do 
-    release_bike = :release_bike
-    it { is_expected.to respond_to release_bike }
+    it { is_expected.to respond_to :release_bike }
+    it "returns a bike instance" do
+      expect(DockingStation.new.release_bike).to be_instance_of(Bike)
+    end
+    it "test if output of release_bike method responds to working? method" do
+      bike_instance = DockingStation.new.release_bike
+      expect(bike_instance).to respond_to(:working?)
+    end
 end
-
