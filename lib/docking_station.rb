be2d2651e1ef9c0cbf33d702389e64db30264849
Rob Oman <rob.oman@icloud.com>
2022-02-24 16:06:07 +0000

    modify release_bike and return_bike methods to raise error if empty/full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,16 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+
   def release_bike
-    Bike.new
+    return @bike if @bike
+    raise Exception.new "There are no bikes available"
   end
+
+  def return_bike(bike)
+    raise Exception.new "There is already a bike here" if @bike
+    @bike = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,37 @@
 require "docking_station"
 
 describe DockingStation do 
+
     it { is_expected.to respond_to :release_bike }
     it "returns a bike instance" do
-      expect(DockingStation.new.release_bike).to be_instance_of(Bike)
+      subject.return_bike(Bike.new)
+      expect(subject.release_bike).to be_instance_of(Bike)
     end
-    it "test if output of release_bike method responds to working? method" do
-      bike_instance = DockingStation.new.release_bike
-      expect(bike_instance).to respond_to(:working?)
+
+    it ":release_bike should raise and error when the docking station is empty" do
+      expect { subject.release_bike }.to raise_error("There are no bikes available")
     end
+
+    it ":return_bike should raise and error when the docking station is empty" do
+      subject.return_bike(Bike.new)
+      expect { subject.return_bike(Bike.new) }.to raise_error("There is already a bike here")
+    end
+
+    it "test if output of Bike instance responds to working? method" do
+      expect(Bike.new).to respond_to(:working?)
+    end
+
+    it ":return_bike should return the bike instance that was docked" do
+      bike = Bike.new
+      expect(subject.return_bike(bike)).to eq(bike)
+    end
+
+    it { is_expected.to respond_to :bike }
+
+    it "" do
+      bike = Bike.new
+      subject.return_bike(bike)
+      expect(subject.bike).to eq(bike)
+    end
+
 end
